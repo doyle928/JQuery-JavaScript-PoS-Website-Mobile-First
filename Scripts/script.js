99eb0759e65ce146df27943353f07106d17f094b
doyle928 <doyle928@yahoo.com>
2018-02-07 17:04:28 -0500

    adding jquery ui

---
+++ Scripts/script.js
@@ -0,0 +1,10 @@
+$('.sideNavButton').click(function () {
+    $('#sideNav').toggle('slide', {
+        direction: 'left'
+    }, 1000);
+})
+$('#sideNav').click(function () {
+    // $('#sideNav').toggle('slide', {
+    //     direction: 'left'
+    // }, 1000);
+});
