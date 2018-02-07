e6499ecaeba04be9535c1e10e9db7b64e9d20ce8
doyle928 <doyle928@yahoo.com>
2018-02-07 18:58:45 -0500

    remove a tags on sliding nav bar

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
+
+    //moblie sliding nav bar
     $(".sideNavButton").click(function () {
         $("#sideNav").toggle("slide",300);        
     });
     $("#sideNav").click(function () {
         $("#sideNav").toggle("slide",300);        
     });
+
+    //
+    
 });
