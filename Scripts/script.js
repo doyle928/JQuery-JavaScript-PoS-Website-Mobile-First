934b9b101055a698729df1d8fd720d93f9db9fab
Emilie Hill <emiliemhill@gmail.com>
2018-02-08 09:42:52 -0500

    cc and js changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,55 +2,109 @@ $(document).ready(function () {
 
     //moblie sliding nav bar
     $(".sideNavButton").click(function () {
-        $("#sideNav").toggle("slide",300);        
+        $("#sideNav").toggle("slide", 300);
     });
     $("#sideNav").click(function () {
-        $("#sideNav").toggle("slide",300);        
+        $("#sideNav").toggle("slide", 300);
     });
 
     //nav buttons, just add the class name to the html element
-    $(".homeButton").click(function (){
+    $(".homeButton").click(function () {
         $("#main").show();
         $("#everyDayShirtsCollectionPage").hide();
         $("#dressShirtsCollectionPage").hide();
         $("#jeansCollectionPage").hide();
         //$("#cart").hide();
-        //$("#recieptPage").hide();
+        $("#receiptPage").hide();
     });
-    $(".everydayButton").click(function (){
+    $(".everydayButton").click(function () {
         $("#main").hide();
         $("#everyDayShirtsCollectionPage").show();
         $("#dressShirtsCollectionPage").hide();
         $("#jeansCollectionPage").hide();
         //$("#cart").hide();
-        //$("#recieptPage").hide();
+        $("#receiptPage").hide();
     });
-    $(".dressButton").click(function (){
+    $(".dressButton").click(function () {
         $("#main").hide();
         $("#everyDayShirtsCollectionPage").hide();
         $("#dressShirtsCollectionPage").show();
         $("#jeansCollectionPage").hide();
         //$("#cart").hide();
-        //$("#recieptPage").hide();
+        $("#receiptPage").hide();
     });
-    $(".jeanButton").click(function (){
+    $(".jeanButton").click(function () {
         $("#main").hide();
         $("#everyDayShirtsCollectionPage").hide();
         $("#dressShirtsCollectionPage").hide();
         $("#jeansCollectionPage").show();
         //$("#cart").hide();
-        //$("#recieptPage").hide();
+        $("#receiptPage").hide();
     });
-    $(".cartButton").click(function (){
+    $(".cartButton").click(function () {
         $("#main").hide();
         $("#everyDayShirtsCollectionPage").hide();
         $("#dressShirtsCollectionPage").hide();
         $("#jeansCollectionPage").hide();
-        //$("#cart").hide();
-        //$("#recieptPage").hide();
+        $("#cart").hide();
+        $("#receiptPage").hide();
+        $("#paymentChoice").show();
     });
+    $(".payment").click(function () {
+        $("#main").hide();
+        $("#everyDayShirtsCollectionPage").hide();
+        $("#dressShirtsCollectionPage").hide();
+        $("#jeansCollectionPage").hide();
+        $("#cart").hide();
+        $("#receiptPage").hide();
+        $("#paymentButtons").show();
+    })
+    $("#cashButton").click(function () {
+        $("#main").hide();
+        $("#everyDayShirtsCollectionPage").hide();
+        $("#dressShirtsCollectionPage").hide();
+        $("#jeansCollectionPage").hide();
+        $("#cart").hide();
+        $("#receiptPage").hide();
+        $("#paymentButtons").hide();
+        $("#cashPay").show();
+    })
+    $("#creditCardButton").click(function () {
+        $("#main").hide();
+        $("#everyDayShirtsCollectionPage").hide();
+        $("#dressShirtsCollectionPage").hide();
+        $("#jeansCollectionPage").hide();
+        $("#cart").hide();
+        $("#receiptPage").hide();
+        $("#paymentButtons").hide();
+        $("#cashPay").hide();
+        $("#creditCardPay").show();
+
+    })
+    $("#cashPaySubmit").click(function () {
+        $("#main").hide();
+        $("#everyDayShirtsCollectionPage").hide();
+        $("#dressShirtsCollectionPage").hide();
+        $("#jeansCollectionPage").hide();
+        $("#cart").hide();
+        $("#receiptPage").show();
+        $("#paymentButtons").hide();
+        $("#cashPay").hide();
+        $("#creditCardPay").hide();
+    })
+    $("#ccPaySubmit".click(function () {
+        $("#main").hide();
+        $("#everyDayShirtsCollectionPage").hide();
+        $("#dressShirtsCollectionPage").hide();
+        $("#jeansCollectionPage").hide();
+        $("#cart").hide();
+        $("#receiptPage").show();
+        $("#paymentButtons").hide();
+        $("#cashPay").hide();
+        $("#creditCardPay").hide();
+    })
 
     //everyday add to cart buttons
     
 
-});
+)});
