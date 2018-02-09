bbee32e0a4c435211533cce05896205cdb2dc5fc
doyle928 <doyle928@yahoo.com>
2018-02-09 02:25:29 -0500

    fixed a lot

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     $("#creditCardPay").hide();
     $("#cartCounter").hide();
     $("#cartCounter").hide();
+    $("#invoice").hide();
     for (i = 0; i < 12; i++) {
         $(".productCart" + (i + 1)).hide();
         $(".productReceipt" + (i + 1)).hide();
@@ -36,6 +37,8 @@ $(document).ready(function () {
         $("#creditCardPay").hide();
         $(".footer").show();
         $("#receiptPage").hide();
+        $(".cashFormFail").hide();
+        $(".cashFail").hide();
         var which = "";
         which = $(this).attr("class").split(' ').pop();
         if (which == "homeButton") {
@@ -56,49 +59,10 @@ $(document).ready(function () {
     });
     //everyday add to cart buttons
 
-    $("#payNow").click(function () {
-        $("#main").hide();
-        $("#everyDayShirtsCollectionPage").hide();
-        $("#dressShirtsCollectionPage").hide();
-        $("#jeansCollectionPage").hide();
-        $("#cartPage").hide();
-        $("#paymentButtons").hide();
-        $(".cartPayIcon").empty();
-        $(".cartPayIcon").text("shopping_cart");
-        $("#cashPay").hide();
-        $("#creditCardPay").hide();
-        $("#paymentChoice").show();
-    })
-    $("#cashButton").click(function () {
-        $("#main").hide();
-        $("#everyDayShirtsCollectionPage").hide();
-        $("#dressShirtsCollectionPage").hide();
-        $("#jeansCollectionPage").hide();
-        $("#cartPage").hide();
-        $("#paymentButtons").hide();
-        $(".cartPayIcon").empty();
-        $(".cartPayIcon").text("shopping_cart");
-        $("#cashPay").show();
-        $("#creditCardPay").hide();
-        $("#paymentChoice").hide();
-    })
-    $("#creditCardButton").click(function () {
-        $("#main").hide();
-        $("#everyDayShirtsCollectionPage").hide();
-        $("#dressShirtsCollectionPage").hide();
-        $("#jeansCollectionPage").hide();
-        $("#cartPage").hide();
-        $("#paymentButtons").hide();
-        $(".cartPayIcon").empty();
-        $(".cartPayIcon").text("shopping_cart");
-        $("#cashPay").hide();
-        $("#creditCardPay").show();
-        $("#paymentChoice").hide();
-    })
     subTotal = 0;
     // add products to cart
     $(".productBuyButton").click(function () {
-        
+
         var productId = $(this).attr("data-product-id");
         $(".productCart" + productId).show();
         $(".productReceipt" + productId).show();
@@ -106,13 +70,17 @@ $(document).ready(function () {
         $("#cartCounter").show();
         $("#cartCounter > p").html(counter);
         if (counter >= 10) {
-            $("#cartCounter > p").css({ "font-size": "8px" });
+            $("#cartCounter > p").css({
+                "font-size": "8px"
+            });
         } else {
-            $("#cartCounter > p").css({ "font-size": "10px" });
+            $("#cartCounter > p").css({
+                "font-size": "10px"
+            });
         }
         var addingTotal = $(".productCart" + productId).find("h3").text();
         addingTotal = addingTotal.substring(1, addingTotal.length);
-        addingTotal = parseInt(addingTotal); 
+        addingTotal = parseInt(addingTotal);
         subTotal += addingTotal;
         console.log(subTotal);
     });
@@ -129,16 +97,64 @@ $(document).ready(function () {
             $("#cartCounter").hide();
         }
         if (counter >= 10) {
-            $("#cartCounter > p").css({ "font-size": "8px" });
+            $("#cartCounter > p").css({
+                "font-size": "8px"
+            });
         } else {
-            $("#cartCounter > p").css({ "font-size": "10px" });
+            $("#cartCounter > p").css({
+                "font-size": "10px"
+            });
         }
         var addingTotal = $(".productCart" + productId).find("h3").text();
         addingTotal = addingTotal.substring(1, addingTotal.length);
-        addingTotal = parseInt(addingTotal); 
+        addingTotal = parseInt(addingTotal);
         subTotal -= addingTotal;
         console.log(subTotal);
     });
+
+    $("#cashButton").click(function () {
+        $("#cashTotalBefore > .typeTo").empty();
+        $("#main").hide();
+        $("#everyDayShirtsCollectionPage").hide();
+        $("#dressShirtsCollectionPage").hide();
+        $("#jeansCollectionPage").hide();
+        $("#cartPage").hide();
+        $("#paymentButtons").hide();
+        $(".cartPayIcon").empty();
+        $(".cartPayIcon").text("shopping_cart");
+        $("#cashPay").show();
+        $("#creditCardPay").hide();
+        $("#paymentChoice").hide();
+        $(".cashFormFail").hide();
+        $(".cashFail").hide();
+        $("#cashTotalBefore > .typeTo").append("$" + subTotal);
+        var salesTax = $("#cashSalesTax > .typeTo").text();
+        salesTax = parseInt(salesTax) / 100 + 1;
+        var totalRound = Math.round((subTotal * salesTax) * 100) / 100;
+        $("#cashPayTotal > .typeTo").text("$" + totalRound);
+    })
+    $("#creditCardButton").click(function () {
+        $("#cashTotalBeforeCC > .typeTo").empty();
+        $("#main").hide();
+        $("#everyDayShirtsCollectionPage").hide();
+        $("#dressShirtsCollectionPage").hide();
+        $("#jeansCollectionPage").hide();
+        $("#cartPage").hide();
+        $("#paymentButtons").hide();
+        $(".cartPayIcon").empty();
+        $(".cartPayIcon").text("shopping_cart");
+        $("#cashPay").hide();
+        $("#creditCardPay").show();
+        $("#paymentChoice").hide();
+        $(".nameFail").hide();
+        $(".cardNumFail").hide();
+        $(".cvvFail").hide();
+        $("#cashTotalBeforeCC > .typeTo").append("$" + subTotal);
+        var salesTax = $("#cashSalesTaxCC > .typeTo").text();
+        salesTax = parseInt(salesTax) / 100 + 1;
+        var totalRound = Math.round((subTotal * salesTax) * 100) / 100;
+        $("#cashPayTotalCC > .typeTo").text("$" + totalRound);
+    })
 
     $("#ccPayButton").click(function (event) {
         var owner = $("#owner").val();
@@ -147,50 +163,83 @@ $(document).ready(function () {
         var expireYear = $("#year").val();
         var cvv = $("#cvv").val();
         event.preventDefault();
-        if (owner != null || owner != "") {
-            if (ccNumber != null || ccNumber != "") {
-                if (expireMonth != null || expireMonth != "") {
-                    if (expireYear != null || expireYear != "") {
-                        if (cvv != null || cvv != "") {
-                            $("#receiptPage").show();
-                            $("#paymentButtons").hide();
-                            $("#cashPay").hide();
-                            $("#creditCardPay").hide();
-                            $("#paymentChoice").hide();
-                        } else {
-                            alert("");
-                        }
-                    } else {
-                        alert("");
-                    }
+
+        var salesTax = $("#cashSalesTaxCC > .typeTo").text();
+        salesTax = parseInt(salesTax) / 100 + 1;
+        console.log(subTotal);
+        var total = subTotal * salesTax;
+
+        if (owner != null && owner != "") {
+            if (ccNumber != null && ccNumber != "" && ccNumber.length != 15) {
+                if (cvv != null && cvv != "") {
+                    $("#receiptPage").show();
+                    $("#paymentButtons").hide();
+                    $("#cashPay").hide();
+                    $("#creditCardPay").hide();
+                    $("#paymentChoice").hide();
+                    $(".nameFail").hide();
+                    $(".cardNumFail").hide();
+                    $(".cvvFail").hide();
+                    $("#invoice").show();
+                    $("#invoiceCashChange").hide();
+                    $("#invoiceCashPaid").hide();
+                    $("#invoicePayment > .invoiceWrite").text("Credit Card");
+                    $("#invoiceTotal > .invoiceWrite").text("$" + (Math.round(total * 100) / 100));
+                    ccNumber = ccNumber.substring(15,19);
+                    $("#invoiceCard > .invoiceWrite").text("-" + ccNumber);
+
                 } else {
-                    alert("");
+                    $(".nameFail").hide();
+                    $(".cardNumFail").hide();
+                    $(".cvvFail").show();
                 }
             } else {
-                alert("");
+                $(".nameFail").hide();
+                $(".cardNumFail").show();
+                $(".cvvFail").hide();
             }
         } else {
-            alert("");
+            $(".nameFail").show();
+            $(".cardNumFail").hide();
+            $(".cvvFail").hide();
         }
+
     })
-    total = 0;
-
-    function salesTax(total) {
-        total = subTotal * 1.06;
-        console.log(total);
-        return total;
-    }
-
-    total = salesTax(subTotal);
     $(".cashPaySubmit").click(function () {
         var cashGiven = $("#cashTendered").val();
+        var salesTax = $("#cashSalesTaxCC > .typeTo").text();
+        salesTax = parseInt(salesTax) / 100 + 1;
+        console.log(subTotal);
+        console.log(cashGiven);
+        var total = subTotal * salesTax;
         if (cashGiven != null || cashGiven != "") {
             if (cashGiven >= total) {
                 var change = cashGiven - total;
-                $("#cashPay").hide();
+                change = Math.round(change * 100) / 100;
+                console.log(change);
+                $(".invoiceWrite").empty();
                 $("#receiptPage").show();
-                $("#receiptPage > p").append(change);
+                $("#paymentButtons").hide();
+                $("#cashPay").hide();
+                $("#creditCardPay").hide();
+                $("#paymentChoice").hide();
+                $("#invoice").show();
+                $("#invoiceCard").hide();
+                $(".cashFormFail").hide();
+                $(".cashFail").hide();
+                $("#invoicePayment > .invoiceWrite").text("Cash");
+                $("#invoiceTotal > .invoiceWrite").text("$" + (Math.round(total * 100) / 100));
+                $("#invoiceCashPaid > .invoiceWrite").text("$" + cashGiven);
+                $("#invoiceCashChange > .invoiceWrite").text("$" + change);
+            } else {
+                $(".cashFormFail").hide();
+                $(".cashFail").show();
             }
+        } else {
+            $(".cashFormFail").show();
+            $(".cashFail").hide();
         }
-    })
+    });
+    total = 0;
+    subTotal = 0;
 });
